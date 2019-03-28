daf2bfc743a0bf0e8348e13aff20ed02ed0f3e6b
fatmanur <fatmanur@DESKTOP-25CTRLM>
2019-03-28 06:03:25 +0300

    homework3

--- cethw1/Models/stcontext.cs
+++ cethw1/Models/stcontext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace cethw1.Models
 {
     public class stcontext : DbContext
---
+++ cethw1/Views/Shared/components/depMenu/depMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using cethw1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cethw1.Views.Shared.components.depMenu
+{
+    public class depMenuViewComponent : ViewComponent
+    {
+        private readonly stcontext dbContext;
+
+
+        public depMenuViewComponent(stcontext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await dbContext.departments.ToListAsync();
+            return View(categories);
+        }
+    }
+}
